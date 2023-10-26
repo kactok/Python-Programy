fdeaebd2636924332c04c38c6da5eebfd1f21b64
kactok <104381556+kactok@users.noreply.github.com>
2023-10-26 19:05:00 +0200

    Initiate the main file

---
+++ main.py
@@ -0,0 +1,14 @@
+# Morse code wiki: https://en.wikipedia.org/wiki/Morse_code
+# Program that converts any String input into Morse Code
+
+# TODO: Read about morse code
+# TODO: take and input from user
+# TODO: Create a function that takes input and converts it into morse code
+
+
+# 26 basic Latin letters(a to z) + Arabic numbers
+# There is no distinction between upper and loser case letters
+# Output:
+# - words displayed as combination of tuples (letters consisting of '-' and '.') (one word is a list of tuples)
+# - space between letters as '...' - 3 dots
+# - space between words as '.......' - 7 dots
