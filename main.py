26f574ff24b3c0f0899ebefed889a201a7951436
kactok <104381556+kactok@users.noreply.github.com>
2023-10-27 13:29:23 +0200

    Add some code

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 # Morse code wiki: https://en.wikipedia.org/wiki/Morse_code
 # Program that converts any String input into Morse Code
-
-# TODO: Read about morse code
-# TODO: take and input from user
-# TODO: Create a function that takes input and converts it into morse code
-
-
 # 26 basic Latin letters(a to z) + Arabic numbers
 # There is no distinction between upper and loser case letters
 # morse code is included in morsecode.py as a dictionary
