25814ca70834889f754dee7ef5c71f977e372325
kactok <104381556+kactok@users.noreply.github.com>
2023-10-27 13:51:45 +0200

    Add some code

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # Morse code wiki: https://en.wikipedia.org/wiki/Morse_code
 # Program that converts any String input into Morse Code
-# 26 basic Latin letters(a to z) + Arabic numbers
-# There is no distinction between upper and loser case letters
+# Rules:
+# 1. 26 basic Latin letters(a to z) + Arabic numbers + . and '
+# 2. There is no distinction between upper and loser case letters
+# 3. Words displayed as combination of '-' and '.'
+# 4. Space between letters as ' ' - 3 dots
+# 5. Space between words as '/' - 7 dots
+
 # morse code is included in morsecode.py as a dictionary
-# Output:
-# - words displayed as combination of '-' and '.' (one word is a list of letters)
-# - space between letters as '...' - 3 dots
-# - space between words as '.......' - 7 dots
 
 from morsecode import morse_code as data
 
@@ -14,21 +15,20 @@ from morsecode import morse_code as data
 morse_code_dict = data
 
 
-def convert(list_of_words: []) -> []:
-    """converts a list of words into morse code"""
-    morse_code_list = []
-    for word in list_of_words:
-        new_word = []
-        for letter in word:
-            letter_as_mc = morse_code_dict.get(letter)
-            new_word.append(letter_as_mc)
-        morse_code_list.append(new_word)
-    return morse_code_list
+def convert(list_of_letters: []) -> str:
+    """converts a list of letters into morse code"""
+    morse_code_sentence = ""
+    for letter in list_of_letters:
+        if letter == " ":
+            morse_code_sentence += '/ '
+        else:
+            morse_code_sentence += f"{str(morse_code_dict.get(letter))} "
+    return morse_code_sentence
 
 
 # User input (all letters lowered)
 user_sentence = input('Enter the sentence to be converted into morse code: ').lower()
-sentence_as_list = user_sentence.split()
-print(sentence_as_list)
+sentence_as_list = list(user_sentence.strip())
+# print(sentence_as_list)
 output = convert(sentence_as_list)
 print(output)
